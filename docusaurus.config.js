0e23793dc45aa16a49730f876ba6392c07df020c
Flouse <1297478+Flouse@users.noreply.github.com>
2023-06-28 14:02:42 +0000

    Remove algolia search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,11 @@
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
+/**
+ * @type {import('@docusaurus/types').DocusaurusConfig}
+ * 
+ * @see https://docusaurus.io/docs/api/docusaurus-config
+ */
 module.exports = {
   title: "Godwoken Docs",
-  tagline: "Godwoken Docs",
+  tagline: "It's built with various articles about Godwoken, from simple concept explanations and step-by-step tutorials, to deep dives into how Godwoken actually works, all waiting for you to explore.",
   url: "https://docs.godwoken.io",
   baseUrl: "/",
   trailingSlash: false,
@@ -9,20 +13,19 @@ module.exports = {
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
   organizationName: "godwokenrises", // Usually your GitHub org/user name.
-  projectName: "godwoken-doc", // Usually your repo name.
-
+  projectName: "godwoken-doc",       // Usually your repo name.
 
   themeConfig: {
     colorMode: {
-      defaultMode: "light",
+      defaultMode: "dark",
       disableSwitch: false,
       respectPrefersColorScheme: false,
     },
-    algolia: {
-      appId: "W0P1A076NN",
-      apiKey: "0180602fbb3e258d02a3c959074d3b04",
-      indexName: "godwoken",
-    },
+    // algolia: {
+    //   appId: "W0P1A076NN",
+    //   apiKey: "0180602fbb3e258d02a3c959074d3b04",
+    //   indexName: "godwoken",
+    // },
     navbar: {
       hideOnScroll: true,
       title: "Godwoken Docs",
@@ -57,10 +60,10 @@ module.exports = {
     [
       "@docusaurus/preset-classic",
       {
-        googleAnalytics: {
-          trackingID: "UA-215912231-1",
-          anonymizeIP: true,
-        },
+        // googleAnalytics: {
+        //   trackingID: "UA-215912231-1",
+        //   anonymizeIP: true,
+        // },
         docs: {
           path: "docs",
           lastVersion: 'current',
